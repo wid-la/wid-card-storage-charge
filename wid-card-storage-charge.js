d96f44478b8538a17c44e0d6745249fb8d475aa7
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-08 13:56:49 -0700

    Styling Element.

--- wid-card-storage-charge.js
+++ wid-card-storage-charge.js
@@ -8,7 +8,61 @@
 
     behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
 
-    properties: {}
+    properties: {
+
+      /**
+       * name
+       * @type {String}
+       */
+      name: {
+        type: String
+      },
+
+      /**
+       * icon
+       * @type {String}
+       */
+      icon: {
+        type: String
+      },
+
+      /**
+       * month_names
+       * @type {Object}
+       */
+      monthNames: {
+        type: Object
+      },
+
+      /**
+       * month01
+       * @type {Object}
+       */
+      month01: {
+        type: Object
+      },
+
+      /**
+       * month02
+       * @type {Object}
+       */
+      month02: {
+        type: Object
+      },
+
+      /**
+       * month03
+       * @type {Object}
+       */
+      month03: {
+        type: Object
+      }
+
+    },
+
+    _getCurrencyValue: function _getCurrencyValue(value) {
+      return '$' + value;
+    }
 
   });
 })();
