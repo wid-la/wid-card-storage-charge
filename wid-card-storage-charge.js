58c2e7ed2bd2829675f4bb294f885dc66e5bd499
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-08 11:47:21 -0700

    Initial Commit.

---
+++ wid-card-storage-charge.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+
+  Polymer({
+
+    is: 'wid-card-storage-charge',
+
+    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
+
+    properties: {}
+
+  });
+})();
